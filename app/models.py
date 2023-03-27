28f1b29de340535f5a51ccce22610ff5b9e4f837
Nikolai Babuhin <nikolai.babuxin@gmail.com>
2023-03-28 02:22:28 +0300

    [FEAT] back 1

--- app/models.py
+++ app/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 
-class AudioFile(models.Model):
+class Video(models.Model):
   project_name = models.CharField(max_length=500)
-  timeline = models.CharField(max_length=100)
-  record = models.CharField(max_length=1000)
+  video = models.CharField(max_length=1000)
 
 
--- srvssft/settings.py
+++ srvssft/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 #APPEND_SLASH=False
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'srvssft',
-    'app'
+    'app',
+
 ]
 
 MIDDLEWARE = [
