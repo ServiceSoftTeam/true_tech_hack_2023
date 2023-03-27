055eb375fbc1b89c57a095b9dbbd57c2a030f257
elizavetatulubenskaya <halvetta@bk.ru>
2023-03-28 02:30:46 +0300

    [FEAT]

--- app/models.py
+++ app/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Video(models.Model):
   project_name = models.CharField(max_length=500)
-  video = models.CharField(max_length=1000)
+  video = models.FileField(upload_to='video')
 
 
