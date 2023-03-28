97f999f9df1f5ea649ecb482aa370ca240c40e88
halvetal <75677306+halvetal@users.noreply.github.com>
2023-03-28 17:35:06 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from srvssft.srvssft.settings import BASE_DIR
 from .models import *
 import requests
-
-FRONT_IP = 'http://82.148.16.205:8001'
+from analytics.audioready_gpt_senteces import GetGPTSentences
+from analytic.audio_analytics import CreateAudio
 class VideoApi(APIView):
     def post(self,request):
         project_name = request.data['project_name']
@@ -14,40 +14,8 @@ class VideoApi(APIView):
             project_name=project_name,
             video=video,
         )
-        return Response({'': ''})
-    async def f(self,request):
-        data = {}
-        requests.post(f"""{FRONT_IP}/""", json=data)
+        kek = GetGPTSentences()
+        audio = CreateAudio(kek)
 
-# Create your views here.
-# class AudioSaveApi(APIView):
-#     def post(self,request):
-#         project_name = request.data['project_name']
-#         timeline = request.data['timeline']
-#         print(request.data)
-#         record = {}
-#         for i in request.data.keys():
-#             if i != 'project_name' and i != 'timeline':
-#                 record[i] = request.data[i]
-#         audio_new = AudioFile.objects.create(
-#             project_name=project_name,
-#             timeline=timeline,
-#             record=record
-#         )
-#         return Response({'': ''})
-#
-# class AudioAllApi(APIView):
-#     def get(self,request):
-#         lst = list(AudioFile.objects.all().values())
-#         return Response(lst)
-#
-#
-# class AudioApi(APIView):
-#     def post(self,request):
-#         project_name = request.data['project_name']
-#         lst = list(AudioFile.objects.all().values())
-#         for i in lst:
-#             if i['project_name'] == project_name:
-#                 return Response(i)
-#         return Response(lst)
-#
+        return Response({'video': f'{BASE_DIR}/video', 'audio':audio})
+
