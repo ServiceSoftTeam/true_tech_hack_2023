28fe3a1397aa67f487dd777b974d2fd47eb49898
halvetal <75677306+halvetal@users.noreply.github.com>
2023-03-29 14:46:56 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,19 +3,25 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from srvssft.srvssft.settings import BASE_DIR
 from .models import *
+from django.core.files.base import ContentFile
 import requests
 from analytics.audioready_gpt_senteces import GetGPTSentences
 from analytic.audio_analytics import CreateAudio
+
 class VideoApi(APIView):
     def post(self,request):
         project_name = request.data['project_name']
-        video = request.FILE['video']
+        video = request.FILE.get('video')
+        video_content = ContentFile(video.read())
         audio_new = Video.objects.create(
             project_name=project_name,
             video=video,
         )
         kek = GetGPTSentences()
         audio = CreateAudio(kek)
+        audio_path = f"""{BASE_DIR}/media/{audio.name}"""
+        with open(audio_path, 'wb+') as destination:
+            for chunk in audio.chunks():
+                destination.write(chunk)
 
-        return Response({'video': f'{BASE_DIR}/video', 'audio':audio})
-
+        return Response({'video': f'{BASE_DIR}/video/{video.name}', 'audio':audio_path})
